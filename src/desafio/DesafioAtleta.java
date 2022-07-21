e2eac6d2c662e91bf5cc1b8dca0a775dacf80c1c
Gabriel Rodrigues <gabrielrf.dev2050@gmail.com>
2022-07-21 20:35:36 -0300

    inserção de Dados

--- src/desafio/DesafioAtleta.java
+++ src/desafio/DesafioAtleta.java
@@ -8,17 +8,35 @@ public class DesafioAtleta {
 
 		int quantidadeDeAtletas;
 		int iCont;
+		double altura;
+		double peso;
 		String name;
+		char sexo;
 
-		System.out.println("Digite a quantidade de Atletas");
-		quantidadeDeAtletas = sc.nextInt();
+		// System.out.println("Digite a quantidade de Atletas");
+		// quantidadeDeAtletas = sc.nextInt();
+		System.out.println("Qual o Nome do Atleta:");
+		name = sc.nextLine();
+		System.out.println("Sexo:\n");
+		sexo = sc.next().charAt(0);
+		System.out.println("Digite a Altura:\n");
+		altura = sc.nextDouble();
+		System.out.println("Digite o Peso");
+		peso = sc.nextDouble();
 
-		for (iCont = 1 ; iCont <= quantidadeDeAtletas ; iCont++){
-			System.out.println("Qual o Nome do Atleta:\n");
-			name=sc.nextLine(); 
+		if (altura < 0) {
+			System.out.println("Digite uma altura válida");
+			altura = sc.nextDouble();
+		}
+
+		if (peso < 0) {
+			System.out.println("Digite uma altura válida");
+			altura = sc.nextDouble();
 		}
 		
-		
+		if (sexo != 'M' || sexo != 'm' || sexo != 'F'|| sexo != 'f' ) {
+			System.out.println("Digite um Sexo Válido"); 
+		}
 
 		sc.close();
 	}
